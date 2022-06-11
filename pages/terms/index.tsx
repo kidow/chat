b0bddeb542c31a5c7836018bab6d3af0ded6d81b
kidow <wcgo2ling@gmail.com>
2022-06-12 04:07:35 +0900

    이용약관, 개인정보처리방침 SEO

---
+++ pages/terms/index.tsx
@@ -0,0 +1,13 @@
+import { SEO } from 'components'
+
+interface State {}
+
+const TermsPage = () => {
+  return (
+    <>
+      <SEO title="이용약관" />
+    </>
+  )
+}
+
+export default TermsPage
---
+++ pages/terms/privacy.tsx
@@ -0,0 +1,13 @@
+import { SEO } from 'components'
+
+interface State {}
+
+const PrivacyPage = () => {
+  return (
+    <>
+      <SEO title="개인정보처리방침" />
+    </>
+  )
+}
+
+export default PrivacyPage
