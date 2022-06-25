24216cce2655664a641f8fb5aa224657d4e2ee6c
kidow <wcgo2ling@gmail.com>
2022-06-25 12:11:52 +0900

    Portal 삭제

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,3 @@
-export { default as Portal } from './Portal'
 export { default as Spinner } from './Spinner'
 export { default as Button } from './Button'
 export { default as Backdrop } from './Backdrop'
